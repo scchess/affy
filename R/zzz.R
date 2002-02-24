374dcafba9069f0c3ef19f2f864d32c23a8a174c
dfcimm2 <dfcimm2@bc3139a8-67e5-0310-9ffc-ced21a209358>
2002-02-24 13:43:22 +0000

    'Cdf' is now a S4 class (the rest modified accordingly) 'PPSet' is also a S4 class...
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@512 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,24 @@
 .First.lib <- function(libname, pkgname, where) {
+  debug.affy123 <- T #DEBUG
   library.dynam("affy", pkgname, libname)
   require(methods)
   where <- match(paste("package:", pkgname, sep=""), search())
+  if (debug.affy123) cat("-->initCdf\n")
+  .initCdf(where)
+  
+  if (debug.affy123) cat("-->initCel\n")
   .initCel(where)
+  if (debug.affy123) cat("-->initCel.container\n")
   .initCel.container(where)
+  
+  if (debug.affy123) cat("-->initPPSet\n")
+  .initPPSet(where)
+  if (debug.affy123) cat("-->initPPSet.container\n")
+  .initPPSet.container(where)
+  if (debug.affy123) cat("-->initPlob\n")
+  .initPlob(where)
+  
+  rm(debug.affy123)
   cacheMetaData(as.environment(where))
 }
 
