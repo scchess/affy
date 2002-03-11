2006cf0ca978f8e1979244115e42b2c3476b4b1b
dfcimm2 <dfcimm2@bc3139a8-67e5-0310-9ffc-ced21a209358>
2002-03-11 17:58:56 +0000

    fixed various bugs ... LG
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@649 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,20 @@
 .First.lib <- function(libname, pkgname, where) {
-  debug.affy123 <- T #DEBUG
+  
+  ## DEBUG flag
+  assign("debug.affy123", T, envir=.GlobalEnv)
+
   library.dynam("affy", pkgname, libname)
   require(methods)
   require(modreg)
   require(eda)
   where <- match(paste("package:", pkgname, sep=""), search())
+  
   if (debug.affy123) cat("-->initCdf\n")
   .initCdf(where)
-  
   if (debug.affy123) cat("-->initCel\n")
   .initCel(where)
   if (debug.affy123) cat("-->initCel.container\n")
   .initCel.container(where)
-  
   if (debug.affy123) cat("-->initPPSet\n")
   .initPPSet(where)
   if (debug.affy123) cat("-->initPPSet.container\n")
@@ -20,7 +22,8 @@
   if (debug.affy123) cat("-->initPlob\n")
   .initPlob(where)
   
-  rm(debug.affy123)
+  ##rm("debug.affy123", envir=where)
+  
   cacheMetaData(as.environment(where))
 }
 
