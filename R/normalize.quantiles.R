3e8ee32bd52b75f6575703f4d81c8029cafae4a0
dfcimm1 <dfcimm1@bc3139a8-67e5-0310-9ffc-ced21a209358>
2002-04-09 03:59:16 +0000

    RI: a couple of final changes. added many expamples to check for errors
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@922 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/normalize.quantiles.R
+++ R/normalize.quantiles.R
@@ -47,3 +47,76 @@ normalize.quantiles <- function(x){
   
   matrix(.C("qnorm_c",as.double(as.vector(x)),as.integer(rows),as.integer(cols))[[1]],rows,cols)
 }
+
+
+normalize.quantiles.robust <- function(x,weights=NULL,remove.extreme=c("variance","mean","both","none"),n.remove=1,approx.meth = FALSE,...){
+  
+  calc.var.ratios <- function(x){
+    cols <- dim(x)[2]
+    vars <- apply(x,2,var)
+    results <- matrix(0,cols,cols)
+    for (i in 1:cols-1)
+      for (j in (i+1):cols){
+        results[i,j] <- vars[i]/vars[j]
+        results[j,i] <- vars[j]/vars[i]
+      }
+    results
+  }
+
+  calc.mean.dists <- function(x){
+    cols <- dim(x)[2]
+    means <- apply(x,2,mean)
+    results <- matrix(0,cols,cols)
+    for (i in 1:cols-1)
+      for (j in (i+1):cols){
+        results[i,j] <- means[i] - means[j]
+        results[j,i] <- means[j] - means[i]
+      }
+    results
+  }
+  
+  rows <- dim(x)[1]
+  cols <- dim(x)[2]
+  
+  if (is.null(weights)){
+    weights <- rep(1,cols)
+    if (remove.extreme == "variance"){
+      var.ratios <- calc.var.ratios(x)
+      vars.big <- apply(var.ratios,1,sum)
+      vars.small <- apply(var.ratios,2,sum)
+      var.adj <- vars.big + vars.small
+      remove.order <- order(-var.adj)
+      weights[remove.order[1:n.remove]] <- 0
+    }
+    if (remove.extreme == "mean"){
+      means <- abs(apply(calc.mean.dists(x),2,sum))
+      remove.order <- order(-means)
+      weights[remove.order[1:n.remove]] <- 0
+    }
+    if (remove.extreme == "both"){
+      var.ratios <- calc.var.ratios(x)
+      vars.big <- apply(var.ratios,1,sum)
+      vars.small <- apply(var.ratios,2,sum)
+      var.adj <- vars.big + vars.small
+      means <- abs(apply(calc.mean.dists(x),2,sum))
+      # by convention we will remove first the most extreme variance, then the most extreme mean
+      remove.order <- order(-var.adj)
+      weights[remove.order[1]] <- 0
+      remove.order <- order(-means)
+      weights[remove.order[1]] <- 0
+    }
+  }
+  if (length(weights) != cols){
+    stop("Weights vector incorrect length\n")
+  }
+  if (sum(weights > 0) < 2){
+    stop("Need at least two non negative weights\n")
+  }
+  cat("Chip weights are ",weights,"\n") 
+  if (approx.meth == FALSE){
+    matrix(.C("qnorm_robust_c",as.double(as.vector(x)),as.double(weights),as.integer(rows),as.integer(cols))[[1]],rows,cols)
+  } else {
+    cat("Approximation currently not implemented \nFalling back to standard Quantile method\n")
+    matrix(.C("qnorm_robust_c",as.double(as.vector(x)),as.double(weights),as.integer(rows),as.integer(cols))[[1]],rows,cols)
+  }
+}
--- src/qnorm.c
+++ src/qnorm.c
@@ -18,6 +18,13 @@ typedef struct{
 } dataitem;
   
 
+int min(int x1,int x2){
+  if (x1 > x2)
+    return x2;
+  else
+    return x1;
+}
+
 int sort_fn(const void *a1,const void *a2){
   dataitem *s1, *s2;
   s1 = (dataitem *)a1;
@@ -70,14 +77,53 @@ void qnorm_c(double *data, int *rows, int *cols){
   double *datvec = malloc(*cols*sizeof(double));
 
 
+
+  /* printf("fred is %i\n",*rows); */
+  
+/*
+  for (i=0; i < *rows; i++){
+    for (j=0; j < *cols; j++){
+      printf("%f ",data[j*(*rows) + i]);
+    }
+    printf("\n");
+  }
+  printf("\n"); */
   /*# sort original columns */
   
   dimat = get_di_matrix(data, *rows, *cols);
   
+  /* qsort(data, (*rows),sizeof(double),(int(*)(const void*, const void*))comp_doub); */
+
+/*  for (i=0; i < *rows; i++){
+    for (j=0; j < *cols; j++){
+      printf("%i ",dimat[j][i].rank);
+    }
+    printf("\n");
+  }
+  printf("\n"); */
+
+  
+
   for (j=0; j < *cols; j++){
     qsort(dimat[j],*rows,sizeof(dataitem),sort_fn);
   }
 
+/*  for (i=0; i < *rows; i++){
+    for (j=0; j < *cols; j++){
+      printf("%f ",dimat[j][i].data);
+    }
+    printf("\n");
+  }
+  printf("\n");
+
+  for (i=0; i < *rows; i++){
+    for (j=0; j < *cols; j++){
+      printf("%i ",dimat[j][i].rank);
+    }
+    printf("\n");
+  }
+
+  printf("\n"); */
   /*# calculate means */
   
   for (i =0; i < *rows; i++){
@@ -87,8 +133,63 @@ void qnorm_c(double *data, int *rows, int *cols){
     qsort(datvec,*cols,sizeof(double),(int(*)(const void*, const void*))sort_double);
     for (j=0; j < *cols; j++){
       sum +=datvec[j]/(double)*cols;;
+      /*printf("%f ",datvec[j]); */
     }
-    row_mean[i] = sum; 
+    /*printf("\n"); */
+    row_mean[i] = sum; /*/(double)*cols;*/
+/*   printf("%f \n",row_mean[i]); */
+  }
+  
+  /*# unsort mean columns */
+  
+  for (i =0; i < *rows; i++){
+    for (j =0; j < *cols; j++){
+      ind = dimat[j][i].rank;
+/*      printf("%i \n",ind);*/
+      data[j*(*rows) + ind] = row_mean[i];
+
+    }
+  }
+  free(dimat);
+  free(row_mean); 
+
+/*  for (i=0; i < *rows; i++){
+    for (j=0; j < *cols; j++){
+      printf("%f ",data[j*(*rows) + i]);
+    }
+    printf("\n");
+  }
+  printf("\n");*/
+}
+
+
+
+void qnorm_robust_c(double *data,double *weights, int *rows, int *cols){
+  int i,j,ind;
+  dataitem **dimat;
+  double sum,sumweights;
+  double *row_mean = malloc((*rows)*sizeof(double));
+  double *datvec = malloc(*cols*sizeof(double));
+
+  dimat = get_di_matrix(data, *rows, *cols);
+  
+  for (j=0; j < *cols; j++){
+    qsort(dimat[j],*rows,sizeof(dataitem),sort_fn);
+  }
+
+  for (i =0; i < *rows; i++){
+    sum = 0.0;
+    for (j=0; j < *cols; j++)
+      datvec[j] = dimat[j][i].data;
+    /* qsort(datvec,*cols,sizeof(double),(int(*)(const void*, const void*))sort_double); */
+    for (j=0; j < (*cols); j++){
+      sum +=weights[j]*datvec[j];
+    }
+    sumweights = 0.0;
+    for (j=0; j < (*cols); j++){
+      sumweights = sumweights + weights[j];
+    }
+    row_mean[i] = sum/sumweights;
   }
   
   /*# unsort mean columns */
@@ -97,11 +198,8 @@ void qnorm_c(double *data, int *rows, int *cols){
     for (j =0; j < *cols; j++){
       ind = dimat[j][i].rank;
       data[j*(*rows) + ind] = row_mean[i];
-
     }
   }
   free(dimat);
   free(row_mean); 
-  free(datvec);
 }
-
