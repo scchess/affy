5922910a0a43a478ba066ffbd52c935fd2889008
dfcimm2 <dfcimm2@bc3139a8-67e5-0310-9ffc-ced21a209358>
2002-08-31 17:41:52 +0000

    various improvements in the tests (ex: "playerout runs on a limited subset of affyid to reduce the testing time) 	-- LG
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@1635 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/expression.values.R
+++ tests/expression.values.R
@@ -8,12 +8,23 @@ cat("Cel.container and Cdf:\n")
 data(CDF.example)
 data(listcel)
 
+## "playerout" very slow. tested individually.
+i <- match("playerout", express.summary.stat.methods)
+express.summary.stat.methods <- express.summary.stat.methods[-i]
+
 for (m in express.summary.stat.methods) {
   for (mbc in bg.correct.methods) {
     cat("expression value with method=", m, "bg correct=", mbc, "...")
     generateExprSet(listcel, CDF.example, method=m, bg.correct=mbc)
     cat("done.\n")
   }
+}
+
+m <- "playerout"
+for (mbc in bg.correct.methods) {
+  cat("expression value with method=", m, "bg correct=", mbc, "...")
+  generateExprSet(listcel, CDF.example, method=m, bg.correct=mbc, ids=CDF.example@name.levels[1:3])
+  cat("done.\n")
 }
 
 ## Plob
