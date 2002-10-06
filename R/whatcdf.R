819b8d01dbd3ec37330903804b7c22205cdc6870
dfcimm2 <dfcimm2@bc3139a8-67e5-0310-9ffc-ced21a209358>
2002-10-06 10:24:08 +0000

    Fix for thecase the CDF name is not found correctly 	-- LG
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@1875 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/whatcdf.R
+++ R/whatcdf.R
@@ -4,8 +4,13 @@ whatcdf <- function(filename, compress=getOption("BioC")$affy$compress.cel){
   tmp <- getInfoInAffyFile(filename,"CEL","HEADER","DatHeader",compress=compress) ##find appropriate line
   tmp <- strsplit(tmp," ")[[1]] #split by space
   tmp <- tmp[grep(".1sq",tmp)] #pick the one with 1sq (from experience)
-  tmp <- gsub("\.1sq","",tmp) #take out .1sq
-  tmp <- gsub("_","",tmp) #take out underscore
-  tmp <- tolower(tmp) #make lower case
+  if (identical(tmp, character(0))) {
+    warning("could not find CDF name, setting it to 'unknown'")
+    tmp <- "unknown"
+  } else {
+    tmp <- gsub("\.1sq","",tmp) #take out .1sq
+    tmp <- gsub("_","",tmp) #take out underscore
+    tmp <- tolower(tmp) #make lower case
+  }
   return(tmp)
 }
