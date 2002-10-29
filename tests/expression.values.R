d6f30a3573472819577857d5ae2048a3a39c8d02
dfcimm2 <dfcimm2@bc3139a8-67e5-0310-9ffc-ced21a209358>
2002-10-29 11:42:52 +0000

    fix ni the test 	-- LG
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@2087 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/expression.values.R
+++ tests/expression.values.R
@@ -1,32 +1,6 @@
 ## routine tests for expression values methods
 library(affy)
 
-## Cel.container and Cdf
-cat("Cel.container and Cdf:\n")
-
-data(CDF.example)
-data(listcel)
-
-## "playerout" very slow. tested individually.
-i <- match("playerout", express.summary.stat.methods)
-meths <- express.summary.stat.methods[-i]
-
-for (m in meths) {
-  for (mbc in bg.correct.methods) {
-    cat("expression value with method=", m, "bg correct=", mbc, "...")
-    generateExprSet(listcel, CDF.example, method=m, bg.correct=mbc)
-    cat("done.\n")
-  }
-}
-
-m <- "playerout"
-for (mbc in bg.correct.methods) {
-  cat("expression value with method=", m, "bg correct=", mbc, "...")
-  generateExprSet(listcel, CDF.example, method=m, bg.correct=mbc, ids=CDF.example@name.levels[1:3])
-  cat("done.\n")
-}
-
-
 ## AffyBatch
 
 cat("AffyBatch:\n")
@@ -51,4 +25,3 @@ for (mbc in bg.correct.methods) {
 }
 
 
-#express()
