0014372ac3558b26da37051f398ed673f9e2a18c
dfcimm2 <dfcimm2@bc3139a8-67e5-0310-9ffc-ced21a209358>
2002-11-01 15:34:13 +0000

    The mas thing for the 'ideal mismatch' 	-- LG
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@2135 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/pmcorrect.mas.R
@@ -0,0 +1,41 @@
+pmcorrect.mas <- function(object, contrast.tau=0.03, scale.tau=10, delta=2^-20)
+  {
+    all.pps.pm <- pm(object)
+    all.pps.mm <- mm(object)
+    
+    diff <- log2(all.pps.pm) - log2(all.pps.mm)
+
+    delta <- rep(delta, nrow(diff))
+    ## segfaults
+    ##sb <- .C("Tukey_Biweight", as.double(diff), as.integer(length(diff)))
+    for (i in 1:ncol(diff)) {
+      sb <- tukey.biweight(diff[, i])
+      pps.pm <- all.pps.pm[, i]
+      pps.mm <- all.pps.mm[, i]
+      pps.im <- pps.mm
+      
+      j <- (pps.mm >= pps.pm) & (sb > contrast.tau)
+      pps.im[j] <- pps.pm[j] / 2^sb
+      
+      j <- (pps.mm >= pps.pm) & (sb <= contrast.tau)
+      pps.im[j] <- pps.pm[j] / 2^(contrast.tau / (1 + (contrast.tau - sb) / scale.tau))
+      
+      pm.corrected <- apply(cbind(pps.pm, pps.im, delta), 1, max)
+      
+      diff[, i] <- pm.corrected
+    }
+    return(diff)
+  }
+
+
+tukey.biweight <- function(x, c=5, epsilon=0.0001)
+  {
+    m <- median(x)
+    s <- median(abs(x - m))
+    u <- (x - m) / (c * s + epsilon)
+    w <- rep(0, length(x))
+    i <- abs(u) <= 1
+    w[i] <- ((1 - u^2)^2)[i]
+    t.bi <- sum(w * x) / sum(w)
+    return(t.bi)
+  }
