903a5d7b7a70a76dab69aec20968b7b83d301d3e
dfcimm2 <dfcimm2@bc3139a8-67e5-0310-9ffc-ced21a209358>
2002-11-02 15:49:35 +0000

    updated the tests (to the recent changes) 	-- LG
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@2159 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/expression.values.R
+++ tests/expression.values.R
@@ -1,26 +1,27 @@
+## -------------------------------------------
 ## routine tests for expression values methods
+## -------------------------------------------
+
 library(affy)
 
-## AffyBatch
-
-cat("AffyBatch:\n")
 data(affybatch.example)
 
 i <- match("playerout", express.summary.stat.methods)
 meths <- express.summary.stat.methods[-i]
 
 for (m in meths) {
-  for (mbc in bg.correct.methods) {
+  for (mbc in pmcorrect.methods) {
      cat("expression value with method=", m, "bg correct=", mbc, "...")
-     computeExprSet(affybatch.example, bg.method=mbc, summary.method=m, warnings=FALSE)
+     computeExprSet(affybatch.example, pmcorrect.method=mbc, summary.method=m, warnings=FALSE)
      cat("done.\n")
    }
 }
 
+## playerout alone 'cause very slow
 m <- "playerout"
-for (mbc in bg.correct.methods) {
+for (mbc in pmcorrect.methods) {
   cat("expression value with method=", m, "bg correct=", mbc, "...")
-  computeExprSet(affybatch.example, bg.method=mbc, summary.method=m, ids=geneNames(affybatch.example)[1:3])
+  computeExprSet(affybatch.example, pmcorrect.method=mbc, summary.method=m, ids=geneNames(affybatch.example)[1:3], warnings=FALSE)
   cat("done.\n")
 }
 
--- tests/normalize.methods.R
+++ tests/normalize.methods.R
@@ -1,36 +1,15 @@
 ## routine tests for the normalization methods
 library(affy)
 
-## Cel.container and Cdf
-cat("Cel.container and Cdf\n")
-
-data(CDF.example)
-data(listcel)
-for (m in normalize.methods(listcel)) {
-  cat("-->", m, "...")
-  listcel.n <- normalize(listcel, CDF.example, method=m)
-  cat("done.\n")
-}
-
-## Plob
-#cat("Plob\n")
-
-#data(Dilution)
-#for (m in normalize.methods(listcel)) {
-#  cat("-->", m, "...")
-#  listcel.n <- normalize(Dilution, method=m)
-#  cat("done.\n")
-#}
-
-## AFfyBatch
-cat("AffyBatch\n")
-
 data(affybatch.example)
+
 n.meth <- normalize.methods(affybatch.example)
+
 ## remove qspline
 n.meth <- n.meth[ ! (n.meth %in% c("pspline"))]
+
 for (m in n.meth) {
-  cat("-->", m, "...")
+  cat("-->method=", m, "...")
   affybatch.example.n <- normalize(affybatch.example, method=m)
   cat("done.\n")
 }
