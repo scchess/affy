9fb925ec812a2e8ca9fe01b43410e85c4753ff73
dfcimm2 <dfcimm2@bc3139a8-67e5-0310-9ffc-ced21a209358>
2002-11-05 14:32:09 +0000

    *** empty log message ***
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@2199 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/generateExprVal.method.mas.R
@@ -0,0 +1,36 @@
+generateExprVal.methods.mas <- function(probes)
+{
+  
+  probes <- log2(probes)
+  
+  rep(NA, ncol(probes))
+  
+  for (i in 1:ncol(probes)) {
+    
+    slg[i] <- tukey.biweight(probes[ ,i])
+    
+  }
+  
+  return(slg)
+    
+}
+
+affy.scalevalue.exprSet <- function(eset, sc=500, analysis)
+{
+  
+  analysis <- match(analysis, c("absolute", "comparison"))
+  
+  if (analysis == "absolute")
+    nf <- 1
+  else
+    stop("not implemented")
+  
+  for (i in 1:ncol(exprs(eset))) {
+    slg <- exprs(eset)[, i]
+    sf <- sc / mean(2^slg, trim=0.02)  
+    reported.value <- nf * sf * 2^slg
+    eset@exprs[, i] <- reported.value
+  }
+  
+  return(eset)
+}
