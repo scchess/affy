ffbd299568e477617ef5a94cd51c6ea291fdd58a
dfcimm2 <dfcimm2@bc3139a8-67e5-0310-9ffc-ced21a209358>
2002-11-12 14:59:39 +0000

    fix to new version 	-- LG
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@2330 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotLocation.R
+++ R/plotLocation.R
@@ -1,10 +1,7 @@
-plotLocation <- function(x, cdf=NULL, nrow=NULL, ncol=NULL, col="green", pch=22, ...) {
-  if (inherits(cdf, "Cdf")) {
-    nrow <- nrow(cdf@name)
-    ncol <- ncol(cdf@name)
-  }
-  if (is.null(nrow) | is.null(ncol)) {
-    stop("nrow and ncol must be specified, explicitly or with a cdf object")
+plotLocation <- function(x, col="green", pch=22, ...) {
+  if (is.list(x)) {
+    x <- cbind(unlist(lapply(x, function(x) x[,1])),
+               unlist(lapply(x, function(x) x[,2])))
   }
   points(x[,1], x[,2]
          , pch=pch, col=col, ...)
