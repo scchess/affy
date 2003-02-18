dd7957146926c5f7886adb490956c89af114e533
dfcimm1 <dfcimm1@bc3139a8-67e5-0310-9ffc-ced21a209358>
2003-02-18 06:38:24 +0000

    bmb - check that X is a matrix
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@3362 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/normalize.quantiles.R
+++ R/normalize.quantiles.R
@@ -24,6 +24,10 @@ normalize.quantiles <- function(x){
 
   rows <- dim(x)[1]
   cols <- dim(x)[2]
+
+  if (!is.matrix(x)){
+    stop("Matrix expected in normalize.quantiles")
+  }
   
   matrix(.C("qnorm_c", as.double(as.vector(x)), as.integer(rows), as.integer(cols))[[1]], rows, cols)
 }
