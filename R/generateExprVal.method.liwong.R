54ee8748880e0bc206919f8386ff4b08fe1630a4
dfcimm1 <dfcimm1@bc3139a8-67e5-0310-9ffc-ced21a209358>
2003-05-15 15:49:07 +0000

    RI: fixed one more thing... need to return a list not a vector when only one probe
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@4508 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/generateExprVal.method.liwong.R
+++ R/generateExprVal.method.liwong.R
@@ -2,7 +2,7 @@ generateExprVal.method.liwong <- function(probes, ...) {
   probes <- t(probes)
   if (ncol(probes) == 1) {
     warning("method liwong unsuitable when only one probe pair")
-    probes
+    list(exprs=as.vector(probes),se.exprs=rep(NA,length(probes)))
   } else {
     tmp <- fit.li.wong(probes, ...)
     list(exprs=tmp$theta,se.exprs=tmp$sigma.theta)
