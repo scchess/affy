b543977f5f6c8cdf52e2999d28dc092a9fb66edf
dfcimm1 <dfcimm1@bc3139a8-67e5-0310-9ffc-ced21a209358>
2003-05-22 04:05:28 +0000

    RI: changed symmetric to gaussian as default in family.loess. this makes it way faster.\n also now use duplicated to avoid warning from approx
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@4611 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/mva.pairs.R
+++ R/mva.pairs.R
@@ -1,25 +1,26 @@
 library(modreg)
-mva.pairs <- function(x,labels=colnames(x),log.it=TRUE,span=2/3,family.loess="symmetric",digits=3,line.col=2,main="MVA plot",...){
+mva.pairs <- function(x,labels=colnames(x),log.it=TRUE,span=2/3,family.loess="gaussian",digits=3,line.col=2,main="MVA plot",...){
   if(log.it) x <-log2(x)
   J <- dim(x)[2]
   frame()
   old.par <- par(no.readonly = TRUE)
-   on.exit(par(old.par))
-par(mfrow=c(J,J),mgp=c(0,.2,0),mar=c(1,1,1,1),oma=c(1,1.4,2,1))
+  on.exit(par(old.par))
+  par(mfrow=c(J,J),mgp=c(0,.2,0),mar=c(1,1,1,1),oma=c(1,1.4,2,1))
   for(j in 1:(J-1)){
     par(mfg=c(j,j));plot(1,1,type="n",xaxt="n",yaxt="n",xlab="",ylab="");text(1,1,labels[j],cex=2)
     for(k in (j+1):J){
       par(mfg=c(j,k))
       yy <- x[,j]-x[,k]
       xx <-(x[,j]+x[,k])/2
-      xx <- xx
-      yy <- yy
       aux <- loess(yy~xx,degree=1,span=span,family=family.loess)$fitted
       plot(xx,yy,pch=".",xlab="",ylab="",tck=0,...)
       o <- order(xx)
-      lines(approx(xx[o],aux[o]),col=line.col)
+      xx <- xx[o]
+      yy <- aux[o]
+      o <-which(!duplicated(xx))
+      lines(approx(xx[o],yy[o]),col=line.col)
       par(mfg=c(k,j))
-      sigma <- quantile(yy,.75)-quantile(yy,.25)
+      sigma <- IQR(yy)
       txt <- format(c(sigma,0.123456789),digits=digits)
       plot(c(0,1),c(0,1),type="n",ylab="",xlab="",xaxt="n",yaxt="n")
       text(0.5,0.5,txt,cex=2)
