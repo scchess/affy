df82a18549178db99e9e6d680aff40cd2e10c4b3
jhnzhang <jhnzhang@bc3139a8-67e5-0310-9ffc-ced21a209358>
2003-10-06 13:15:23 +0000

    expresso.R modified by JZ and expressoWidget.R and Rd file added.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@5924 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/expresso.R
+++ R/expresso.R
@@ -18,69 +18,76 @@ expresso <- function(afbatch,
                      verbose = TRUE,
                      widget = FALSE
                      ) {
-  
-  
-  
+    # JZ added this function
+    setCorrections <- function(){
+        bioc.opt <- getOption("BioC")
+        if(bg.correct){
+            if(is.null(bgcorrect.method)){
+                BGMethods <- bgcorrect.methods
+            }else{
+                BGMethods <- bgcorrect.method
+            }
+        }else{
+            BGMethods <- "None"
+        }
+        if(normalize){
+            if(is.null(normalize.method)){
+                normMethods <- normalize.methods(afbatch)
+            }else{
+                normMethods <- normalizer.method
+            }
+        }else{
+            normMethods <- "None"
+        }
+        # Default for this one may not be correct
+        if(is.null(pmcorrect.method)){
+            PMMethods <- pmcorrect.methods
+        }else{
+            PMMethods <- pmcorrect.method
+        }
+        # Default for this one may not be correct
+        if(is.null(summary.method)){
+            expMethods <- generateExprSet.methods
+
+        }else{
+            expMethods <- summary.method
+        }
+
+        corrections <- expressoWidget(BGMethods, normMethods, PMMethods,
+                           expMethods, bioc.opt$affy$bgcorrect.method,
+                                      bioc.opt$affy$normalize.method,
+                                      bioc.opt$affy$pmcorrect.method,
+                                      bioc.opt$affy$summary.method)
+        if(!is.null(corrections)){
+            if(corrections[["BG"]] != "None"){
+                bgcorrect.method <<- corrections[["BG"]]
+            }
+            if(corrections[["NORM"]] != "None"){
+                normalize.method <<- corrections[["NORM"]]
+            }
+            if(corrections[["PM"]] != "None"){
+                pmcorrect.method <<- corrections[["PM"]]
+            }
+            if(corrections[["EXP"]] != "None"){
+                summary.method <<- corrections[["EXP"]]
+            }
+        }else{
+            stop("Aborted by user")
+        }
+    }
+
   if (widget) {
     require(tkWidgets) || stop("library tkWidgets could not be found !")
   }
-  
+
   nchips <- length(afbatch)
 
   ###background stuff must be added before normalization!
-  
-  ## -- background correction method
-  if (bg.correct && is.null(bgcorrect.method)) {
-    if (widget) {
-      bgcorrect.method <- pwidget.selector(bgcorrect.methods,
-                                    title = "Method for background correction")
-                                    ##,choices.help = NULL)
-      #bg.method <- paste("bg.correct", bg.method, sep=".")
-    } else {
-      stop("bg.method missing")
-    }
-  }
-  
-  ## -- normalize.method
-  if ((normalize) & (is.null(normalize.method))) {
-    if (widget) {
-      ##DEBUG: move what's below to 'pwidget.selector'
-      n.methods <- normalize.methods(afbatch)
-      normalize.method <- pwidget.selector(n.methods,
-                                           title = "Method for normalization")
-      ##choices.help = paste("normalize", n.methods,  sep="."))##took this out cause netscape popping up was annoying me
-      
-      rm(n.methods)
-    } else {
-      stop(paste("normalization method missing. Try one of:",
-                 normalize.methods(afbatch), sep="\n"))
-    }
+
+  if(widget){
+      setCorrections()
   }
 
-  ## -- background correction method
-  if (is.null(pmcorrect.method)) {
-    if (widget) {
-      pmcorrect.method <- pwidget.selector(pmcorrect.methods,
-                                    title = "Method for PM correction")
-      ##,choices.help = NULL)
-      #bg.method <- paste("bg.correct", bg.method, sep=".")
-    } else {
-      stop("pmcorrect.method missing")
-    }
-  }
-  
-  ## -- expression method
-  if (is.null(summary.method)) {
-    if (widget) {
-      helpnames <- paste("generateExprVal.method", generateExprSet.methods, sep="")
-      summary.method <- pwidget.selector(generateExprSet.methods,
-                                         title = "Method for expression")
-                                         ##choices.help = helpnames)
-    } else {
-      stop("summary.method method missing")
-    }
-  }
-  
   ## -- summary of what will be done
   if (verbose) {
     if (bg.correct){
@@ -92,38 +99,38 @@ expresso <- function(afbatch,
     cat("PM/MM correction :", pmcorrect.method, "\n")
     cat("expression values:", summary.method, "\n")
   }
-  
+
 
   ## -- background correct (if needed)
   if (bg.correct) {
-    
+
     if (verbose)
       cat("background correcting...")
-    
+
     afbatch <- do.call("bg.correct", c(alist(afbatch, method=bgcorrect.method), bgcorrect.param))
-    
+
     if (verbose)
       cat("done.\n")
   }
-    
+
   ## -- normalize (if wished)
   if (normalize) {
-    
+
     if (verbose)
       cat("normalizing...")
-    
+
     afbatch <- do.call("normalize",
                        c(alist(afbatch, normalize.method), normalize.param))
-    
+
     if (verbose)
       cat("done.\n")
-  }  
-  
+  }
+
   eset <- computeExprSet(afbatch,
                          summary.method=summary.method, pmcorrect.method= pmcorrect.method,
                          ids=summary.subset,
                          summary.param=summary.param, pmcorrect.param=pmcorrect.param)
-  
+
   return(eset)
 }
 
