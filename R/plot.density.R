75dad5b71f670a6d10b6176b0f7a4096fc57a40e
dfcimm1 <dfcimm1@bc3139a8-67e5-0310-9ffc-ced21a209358>
2003-11-15 14:31:56 +0000

    RI: bug fix so that color argument gets passed
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@6656 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plot.density.R
+++ R/plot.density.R
@@ -14,35 +14,38 @@
                        
 # }
 
-plotDensity <- function(mat,ylab="density", xlab="x", type="l", col=1:6,...){
-
-  x.density <- apply(mat, 2, density)
+plotDensity <- function(mat,
+                        ylab="density", xlab="x", type="l", col=1:6,
+                        ...) {
   
+  x.density <- apply(mat, 2, density)
+
   all.x <- do.call("cbind", lapply(x.density, function(x) x$x))
   all.y <- do.call("cbind", lapply(x.density, function(x) x$y))
-
+  
   matplot(all.x, all.y, ylab=ylab, xlab=xlab, type=type, col=col, ...)
 
   invisible(list(all.x=all.x, all.y=all.y))
 }
-
+ 
 
 plotDensity.AffyBatch <- function(x, col=1:6, log=TRUE,
-                                  which=c("pm","mm","both"),ylab="density",
-                                  xlab=NULL,...){
-
+                                  which=c("pm","mm","both"),
+                                  ylab="density",
+                                  xlab=NULL,
+                                  ...){
+  
   Index <- unlist(indexProbes(x, which=which))
   
   x <- intensity(x)[Index, ,drop=FALSE]
   
   if(log){
-            x <- log2(x)
-            if(is.null(xlab)) xlab <- "log intensity"
-          }
+    x <- log2(x)
+    if(is.null(xlab)) xlab <- "log intensity"
+  }
   else  if(is.null(xlab)) xlab <- "intensity"
   
   rv <- plotDensity(x, ylab=ylab, xlab=xlab, col=col, ...)
-  
+
   invisible(rv)
 }
-
