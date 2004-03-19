f7747d51cde5519193b82477515a0715e8ae794d
dfcimm2 <dfcimm2@bc3139a8-67e5-0310-9ffc-ced21a209358>
2004-03-19 03:50:24 +0000

    Fix for the changes multiget -> mget previously made (expects NA when not found, mget seems to return FALSE by default) 	-- LG
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@7921 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ppsetApply.R
+++ R/ppsetApply.R
@@ -5,7 +5,7 @@ ppsetApply <- function(abatch, FUN, genenames=NULL, ...) {
 
   if (! inherits(FUN, "function"))
     stop("FUN must be a function")
-
+  
   cdfenv <- getCdfInfo(abatch)
 
   if (is.null(genenames))
@@ -15,15 +15,15 @@ ppsetApply <- function(abatch, FUN, genenames=NULL, ...) {
   e1 <- new.env(parent = environment(FUN))
   multiassign(names(pData(abatch)), pData(abatch), env = e1)
   environment(FUN) <- e1
-
+  
   ppset <- new("ProbeSet", pm=matrix(), mm=matrix())
 
   r <- vector("list", length=length(genenames))
   names(r) <- genenames
-
+  
   for (i in seq(along=genenames)) {
     ## use multiget to get NA when genenames[i] not found
-      probes.i <- mget(genenames[i], envir=cdfenv)[[1]]
+    probes.i <- mget(genenames[i], envir = cdfenv, ifnotfound = NA)[[1]]
     if (all(is.na(probes.i)))
       next
     ppset@pm <- intensity(abatch)[probes.i[, 1], , drop=FALSE]
