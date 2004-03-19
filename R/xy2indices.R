f81e41fcebd7bd6b3d1eb39889f8a414c15d2615
dfcimm2 <dfcimm2@bc3139a8-67e5-0310-9ffc-ced21a209358>
2004-03-19 03:52:35 +0000

    On the way to have 'zero-offset' xy-coordinates, a variable offset is introduced (to ensure compatibility with existing work one might have made) 	-- LG
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@7922 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/xy2indices.R
+++ R/xy2indices.R
@@ -1,6 +1,12 @@
-xy2indices <- function(x, y, nr=NULL, cel=NULL, abatch=NULL) {
-  if (any(x <= 0) || any(y <= 0))
-    stop("Xs and Ys must start at 1 (please refer to the help file) !")
+xy2indices <- function(x, y, nr=NULL, cel=NULL, abatch=NULL, xy.offset = NULL) {
+
+  if ( is.null(xy.offset) ) {
+    xy.offset <- getOption("BioC")$affy$xy.offset
+  }
+  
+  if (any(x < xy.offset) || any(y < xy.offset))
+    stop("Xs and Ys must start at 0 or 1 (please refer to the help file) !")
+  
   ct <- sum(c(is.null(nr), is.null(cel), is.null(abatch)))
   if (ct != 2)
     stop("One and only one of 'nr', 'cel', 'abatch' should be specified.")
@@ -9,13 +15,19 @@ xy2indices <- function(x, y, nr=NULL, cel=NULL, abatch=NULL) {
   if (! is.null((abatch)))
     nr <- nrow(abatch)
   
-  return(x + nr * (y - 1))
+  return( (x - xy.offset) + 1 + nr * (y - xy.offset) )
 }
 
-indices2xy <- function(i, nr=NULL, cel=NULL, abatch=NULL) {
-  if (any(i)<= 0)
-    stop("Indices must start at 1 (please refer to the help file) !")
+
+indices2xy <- function(i, nr=NULL, cel=NULL, abatch=NULL, xy.offset = NULL) {
+
+  if ( is.null(xy.offset) ) {
+    xy.offset <- getOption("BioC")$affy$xy.offset
+  }
   
+  if (any(i) <= 0)
+    stop("Indices must start at 0 or 1 (please refer to the help file) !")
+
   ct <- sum(c(is.null(nr), is.null(cel), is.null(abatch)))
   
   if (ct != 2)
@@ -25,9 +37,11 @@ indices2xy <- function(i, nr=NULL, cel=NULL, abatch=NULL) {
   if (! is.null((abatch)))
     nr <- nrow(abatch)
   
-  x <- i %% nr
-  x[x == 0] <- nr
-  y <- (i-1) %/% nr + 1
+  ##x <- i %% nr
+  ##x[x == 0] <- nr
+  ##y <- (i - 1) %/% nr + 1  
+  x <- (i  - 1) %% nr + xy.offset
+  y <- (i - 1) %/% nr + xy.offset
   xy <- cbind(x, y)
   colnames(xy) <- c("x", "y")
   return(xy)
