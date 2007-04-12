f1dcaea42ef1c2b1ea1a763e85d4970dc9c02296
jmacdon <jmacdon@bc3139a8-67e5-0310-9ffc-ced21a209358>
2007-04-12 17:25:36 +0000

    JWM: Added 'cdf' argument to xy2indices() and indices2xy() so those without an AffyBatch can still map things. The cdf argument in this case is simply the name of the requisite cdf package.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@23809 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/xy2indices.R
+++ R/xy2indices.R
@@ -1,4 +1,4 @@
-xy2indices <- function(x, y, nr=NULL, cel=NULL, abatch=NULL, xy.offset = NULL) {
+xy2indices <- function(x, y, nr=NULL, cel=NULL, abatch=NULL, cdf=NULL, xy.offset = NULL) {
 
   if ( is.null(xy.offset) ) {
     xy.offset <- getOption("BioC")$affy$xy.offset
@@ -7,19 +7,25 @@ xy2indices <- function(x, y, nr=NULL, cel=NULL, abatch=NULL, xy.offset = NULL) {
   if (any(x < xy.offset) || any(y < xy.offset))
     stop("Xs and Ys must start at 0 or 1 (please refer to the help file) !")
   
-  ct <- sum(c(is.null(nr), is.null(cel), is.null(abatch)))
-  if (ct != 2)
-    stop("One and only one of 'nr', 'cel', 'abatch' should be specified.")
+  ct <- sum(c(is.null(nr), is.null(cel), is.null(abatch), is.null(cdf)))
+  if (ct != 3)
+    stop("One and only one of 'nr', 'cel', 'abatch', 'cdf' should be specified.")
   if (! is.null(cel))
     stop("Cel class no longer supported") #nr <- nrow(intensity(cel))
   if (! is.null((abatch)))
     nr <- nrow(abatch)
+  if(!is.null(cdf)){
+    require(cdf, character.only = TRUE, quiet = TRUE) ||
+            stop(paste(cdf, "package must be installed first"))
+    nam <- paste(sub("cdf$", "", cdf), "dim", sep = "")
+    nr <- get("NROW", get(nam))
+  }
   
   return( (x - xy.offset) + 1 + nr * (y - xy.offset) )
 }
 
 
-indices2xy <- function(i, nr=NULL, cel=NULL, abatch=NULL, xy.offset = NULL) {
+indices2xy <- function(i, nr=NULL, cel=NULL, abatch=NULL, cdf=NULL, xy.offset = NULL) {
 
   if ( is.null(xy.offset) ) {
     xy.offset <- getOption("BioC")$affy$xy.offset
@@ -28,14 +34,21 @@ indices2xy <- function(i, nr=NULL, cel=NULL, abatch=NULL, xy.offset = NULL) {
   if (any(i) <= 0)
     stop("Indices must start at 0 or 1 (please refer to the help file) !")
 
-  ct <- sum(c(is.null(nr), is.null(cel), is.null(abatch)))
+  ct <- sum(c(is.null(nr), is.null(cel), is.null(abatch), is.null(cdf)))
   
-  if (ct != 2)
-    stop("One and only one of 'nr', 'cel', 'abatch' should be specified.")
+  if (ct != 3)
+    stop("One and only one of 'nr', 'cel', 'abatch', 'cdf' should be specified.")
   if (! is.null(cel))
     stop("Cel class no longer supported")#    nr <- nrow(intensity(cel))
   if (! is.null((abatch)))
     nr <- nrow(abatch)
+  if(!is.null(cdf)){
+    require(cdf, character.only = TRUE, quiet = TRUE) ||
+            stop(paste(cdf, "package must be installed first"))
+    nam <- paste(sub("cdf$", "", cdf), "dim", sep = "")
+    nr <- get("NROW", get(nam))
+  }
+  
   
   ##x <- i %% nr
   ##x[x == 0] <- nr
