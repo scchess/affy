124a78fe19fbc5b4d637db900b66418763282475
laurent@cbs.dtu.dk <laurent@cbs.dtu.dk@bc3139a8-67e5-0310-9ffc-ced21a209358>
2008-06-26 11:26:02 +0000

    - Applied patch by Markus Schmidberger https://stat.ethz.ch/pipermail/bioc-devel/2008-June/001628.html
    - bumped version number
    
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@32447 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/normalize.loess.R
+++ R/normalize.loess.R
@@ -37,14 +37,11 @@ normalize.loess <- function(mat, subset=sample(1:(dim(mat)[1]), min(c(5000, nrow
 
   J <- dim(mat)[2]
   II <- dim(mat)[1]
-  newData <- mat
   if(log.it){
     mat <- log2(mat)
-    newData <- log2(newData)
   }
 
   change <- epsilon +1
-  fs <- matrix(0, II, J)##contains what we substract
   iter <- 0
   w <- c(0, rep(1,length(subset)), 0) ##this way we give 0 weight to the
                                       ##extremes added so that we can interpolate
@@ -55,8 +52,8 @@ normalize.loess <- function(mat, subset=sample(1:(dim(mat)[1]), min(c(5000, nrow
 
     for (j in 1:(J-1)){
       for (k in (j+1):J){
-        y <- newData[,j] - newData[,k]
-        x <- (newData[,j] + newData[,k]) / 2
+        y <- mat[,j] - mat[,k]
+        x <- (mat[,j] + mat[,k]) / 2
         index <- c(order(x)[1], subset, order(-x)[1])
         ##put endpoints in so we can interpolate
         xx <- x[index]
@@ -66,17 +63,14 @@ normalize.loess <- function(mat, subset=sample(1:(dim(mat)[1]), min(c(5000, nrow
         means[, j] <- means[, j] + aux
         means[, k] <- means[, k] - aux
         if (verbose)
-          cat("Done with",j,"vs",k," in iteration ",iter,"\n")
+          cat("Done with",j,"vs",k,"in iteration",iter,"\n")
       }
     }
-    fs <- fs + means
-    newData <- mat - fs
+    mat <- mat - means
     change <- max(colMeans((means[subset,])^2))
 
     if(verbose)
       cat(iter, change,"\n")
-
-    oldfs <- fs
 
   }
 
@@ -84,7 +78,7 @@ normalize.loess <- function(mat, subset=sample(1:(dim(mat)[1]), min(c(5000, nrow
     warning(paste("No convergence after", maxit, "iterations.\n"))
 
   if(log.it) {
-    return(2^newData)
+    return(2^mat)
   } else
-    return(newData)
+    return(mat)
 }
