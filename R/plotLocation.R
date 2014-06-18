9b4ae876d327bc65da7c4b43b6e77c570af97935
jmacdon <jmacdon@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-06-18 16:17:20 +0000

    Forgot to use ceiling on par("usr") to ensure I get the right row.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affy@91576 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotLocation.R
+++ R/plotLocation.R
@@ -7,7 +7,7 @@ plotLocation <- function(x, col="green", pch=22, ...) {
   ## use image width to get nrow, which isn't ideal
   ## but follows assumption for this function that an
   ## image already exists
-  nrow <- par("usr")[4]
+  nrow <- ceiling(par("usr")[4])
   if(nrow == 1) stop(paste("\nYou must first generate an image of an array",
      "for this function to work!\n\n"), call. = FALSE)
   points(x[,1], nrow - x[,2]
